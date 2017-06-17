8e1964d5c503dcb3f9575c485957266ab2a05361
BrunoMine <borgesdossantosbruno@gmail.com>
2017-06-17 14:51:22 -0300

    Ajustes de arte e informativos

--- diretrizes.lua
+++ diretrizes.lua
@@ -7,11 +7,11 @@
 terrenos.var = {}
 
 -- Node usado como pagamento
-terrenos.var.pag = "default:apple"
+terrenos.var.pag = minetest.setting_get("terrenos_item_pag") or "default:gold_ingot"
 terrenos.var.pag_desc = minetest.registered_items[terrenos.var.pag].description
 
 -- Quantidade de nodes pago como alugal
-terrenos.var.qtd_pag_aluguel = 1
+terrenos.var.qtd_pag_aluguel = tonumber(minetest.setting_get("terrenos_qtd_pag_aluguel") or 1)
 
 -- Quantidade de dias para pagar aluguel
-terrenos.var.max_dias = 3 * 30
+terrenos.var.max_dias = tonumber(minetest.setting_get("terrenos_meses_aluguel") or 3) * 30
